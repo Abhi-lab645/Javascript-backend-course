4c7fe68c514f71d6cd48327def62012f21e61932
Abhi-lab645 <abhinavgolu18072005@gmail.com>
2026-02-06 10:33:23 +0530

    async/await start

---
+++ S02.js
@@ -0,0 +1,35 @@
+// ⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️ L-363(async/await)  ⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️ 
+
+/*
+async function myFunc() {
+    return 10;
+}
+
+console.log(myFunc());
+*/
+
+
+/*
+function getData(){
+    return new Promise(function exec(resolve,reject){
+        setTimeout(function data(){
+            resolve("Data received");
+        },3000);
+    });
+}
+
+
+
+
+async function showData() {
+    const result=await getData();
+    console.log(result);
+    return result;
+}
+
+const p=showData();
+*/
+
+
+
+
